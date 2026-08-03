21c9fbb8d757de53194a1188f6a4f99c9082af50
Kai Fukami <62085615+kfukami@users.noreply.github.com>
2026-08-03 16:29:34 +0900

    Update rpca_solver.m

--- functions/rpca_solver.m
+++ functions/rpca_solver.m
@@ -1,3 +1,16 @@
+% rpca_solver.m
+% 2026 Kai Fukami (Tohoku University, kfukami1@tohoku.ac.jp)
+
+% Authors:
+% Pablo Koop, Isabel Scherl, and Kai Fukami
+% We provide no guarantees for this code.  Use as-is and for academic research use only; no commercial use allowed without permission. For citation, please use the reference below:
+%     Ref: Pablo Koop, Isabel Scherl, and Kai Fukami,
+%     “Library-learning-assisted robust principal component analysis for denoising severely corrupted flow fields,”
+%     in Review
+%
+% The code is written for educational clarity and not for speed.
+% -- version 1: Aug 3, 2026
+
 function [L, S, info] = inexact_alm_rpca( ...
         X, lambda_scale, ...
         convergence_tolerance, maximum_iterations)
